26720f2603bc32e4bd681b120b6bdf35f396971f
Prikshit-Rai <raiprikshit01@gmail.com>
2024-09-14 13:55:52 +1000

    DevOps project part4

---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "sit722task82p"
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable kubernetes_version {    
+  default = "1.29.2"
+}
